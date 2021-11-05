15b90a97ec24c871b239b53b1632eefc324ae957
Tiago Gil <73337790+itzgunny@users.noreply.github.com>
2021-11-05 10:52:33 +0000

    CSS fix
    Fixed CSS not working on different named parent directories

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -16,12 +16,12 @@ $customImg = CustomImg::register($this);
 <title>W3.CSS Template</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="/healthify/frontend/web/css/customCss.css">
+<link rel="stylesheet" href="../web/css/customCss.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inconsolata">
 
 <!-- Header with image -->
 <header class="bgimg w3-display-container w3-grayscale-min" id="home">
-    <img src="<?=$customImg->baseUrl.'/menuBg.jpg' ?>" class="customBackground">
+    <img src="<?= $customImg->baseUrl . '/menuBg.jpg' ?>" class="customBackground">
 
     <div class="w3-display-bottomleft w3-center w3-padding-large w3-hide-small">
         <span class="w3-tag">Open from <?php echo date('l jS \of F Y h:i:s A'); ?></span>
@@ -41,13 +41,18 @@ $customImg = CustomImg::register($this);
     <div class="w3-container" id="about">
         <div class="w3-content" style="max-width:700px">
             <h5 class="w3-center w3-padding-64"><span class="w3-tag w3-wide">ABOUT HEALTHIFY</span></h5>
-            <p>The Cafe was founded in Leiria by Mr. Smith in lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <p>In addition to our full espresso and brew bar menu, we serve fresh made-to-order breakfast and lunch sandwiches, as well as a selection of sides and salads and other good stuff.</p>
+            <p>The Cafe was founded in Leiria by Mr. Smith in lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <p>In addition to our full espresso and brew bar menu, we serve fresh made-to-order breakfast and lunch
+                sandwiches, as well as a selection of sides and salads and other good stuff.</p>
             <div class="w3-panel w3-leftbar w3-light-grey">
-                <p><i>"Use products from nature for what it's worth - but never too early, nor too late." Fresh is the new sweet.</i></p>
+                <p><i>"Use products from nature for what it's worth - but never too early, nor too late." Fresh is the
+                        new sweet.</i></p>
                 <p>Chef, Coffeeist and Owner: Liam Brown</p>
             </div>
-            <img src="<?=$customImg->baseUrl.'/placeholder.png' ?>" style="width:100%;max-width:1000px" class="w3-margin-top">
+            <img src="<?= $customImg->baseUrl . '/placeholder.png' ?>" style="width:100%;max-width:1000px"
+                 class="w3-margin-top">
             <p><strong>Opening hours:</strong> everyday from 6am to 5pm.</p>
             <p><strong>Address:</strong> 15 Adr street, 5015, NY</p>
         </div>
@@ -101,7 +106,8 @@ $customImg = CustomImg::register($this);
                 <h5>Soda</h5>
                 <p class="w3-text-grey">Coke, Sprite, Fanta, etc. 2.50</p>
             </div>
-            <img src="<?=$customImg->baseUrl.'/placeholder.png' ?>" style="width:100%;max-width:1000px;margin-top:32px;">
+            <img src="<?= $customImg->baseUrl . '/placeholder.png' ?>"
+                 style="width:100%;max-width:1000px;margin-top:32px;">
         </div>
     </div>
 
@@ -110,20 +116,28 @@ $customImg = CustomImg::register($this);
         <div class="w3-content" style="max-width:700px">
             <h5 class="w3-center w3-padding-48"><span class="w3-tag w3-wide">WHERE TO FIND US</span></h5>
             <p>Find us at some address at some place.</p>
-            <img src="<?=$customImg->baseUrl.'/placeholder.png' ?>" class="w3-image" style="width:100%">
-            <p><span class="w3-tag">FYI!</span> We offer full-service catering for any event, large or small. We understand your needs and we will cater the food to satisfy the biggerst criteria of them all, both look and taste.</p>
+            <img src="<?= $customImg->baseUrl . '/placeholder.png' ?>" class="w3-image" style="width:100%">
+            <p><span class="w3-tag">FYI!</span> We offer full-service catering for any event, large or small. We
+                understand your needs and we will cater the food to satisfy the biggerst criteria of them all, both look
+                and taste.</p>
             <p><strong>Reserve</strong> a table, ask for today's special or just send us a message:</p>
             <form action="" target="_blank">
-                <p><input class="w3-input w3-padding-16 w3-border" type="text" placeholder="Name" required name="Name"></p>
-                <p><input class="w3-input w3-padding-16 w3-border" type="number" placeholder="How many people" required name="People"></p>
-                <p><input class="w3-input w3-padding-16 w3-border" type="datetime-local" placeholder="Date and time" required name="date" value="2020-11-16T18:00"></p>
-                <p><input class="w3-input w3-padding-16 w3-border" type="text" placeholder="Message \ Special requirements" required name="Message"></p>
-                <p><button class="w3-button w3-black" type="submit" disabled>SEND MESSAGE</button></p>
+                <p><input class="w3-input w3-padding-16 w3-border" type="text" placeholder="Name" required name="Name">
+                </p>
+                <p><input class="w3-input w3-padding-16 w3-border" type="number" placeholder="How many people" required
+                          name="People"></p>
+                <p><input class="w3-input w3-padding-16 w3-border" type="datetime-local" placeholder="Date and time"
+                          required name="date" value="2020-11-16T18:00"></p>
+                <p><input class="w3-input w3-padding-16 w3-border" type="text"
+                          placeholder="Message \ Special requirements" required name="Message"></p>
+                <p>
+                    <button class="w3-button w3-black" type="submit" disabled>SEND MESSAGE</button>
+                </p>
             </form>
         </div>
     </div>
 
     <!-- End page content -->
 </div>
-<script src="<?=$customJs->baseUrl.'/customJs.js'?>"></script>
+<script src="<?= $customJs->baseUrl . '/customJs.js' ?>"></script>
 </html>
