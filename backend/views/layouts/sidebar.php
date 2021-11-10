5d8775358281c20042db035ae1587843b279d1f6
Tiago Gil <2201093@my.ipleiria.pt>
2021-11-10 00:18:12 +0000

    Sidebar links, Login Image

--- backend/views/layouts/sidebar.php
+++ backend/views/layouts/sidebar.php
@@ -1,12 +1,15 @@
 <?php
+
 use yii\helpers\URL;
 use yii\helpers\Html;
+
 ?>
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="<?=Url::home()?>" class="brand-link">
-        <img src="<?=$assetDir?>/img/AdminLTELogo.png" alt="Healthify Logo" class="brand-image img-circle elevation-3"
+    <a href="<?= Url::home() ?>" class="brand-link">
+        <img src="<?= $assetDir ?>/img/AdminLTELogo.png" alt="Healthify Logo" class="brand-image img-circle elevation-3"
              style="opacity: .8">
+        <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
         <span class="brand-text font-weight-light">Healthify</span>
     </a>
 
@@ -15,20 +18,49 @@ use yii\helpers\Html;
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="<?=$assetDir?>/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                <img src="<?= $assetDir ?>/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block"><?=Yii::$app->getUser()->identity->getName()?></a>
+                <a href="#" class="d-block"><?= Yii::$app->getUser()->identity->getName() ?></a>
             </div>
         </div>
 
         <!-- Sidebar Menu -->
+
         <nav class="mt-2">
-
-            <a href="#" class="btn"></a>
-            <?= Html::a('Sign out', ['site/logout'], ['data-method' => 'post', 'class' => 'btn btn-default btn-flat float-right']) ?>
-            <?= HTML::a('UserManager', ['/user/index'], ['data-method' => 'post', 'class' => 'btn btn-default btn-flat float-right']) ?>
-
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu">
+                <!-- Add icons to the links using the .nav-icon class
+                     with font-awesome or any other icon font library -->
+                <li class="nav-item has-treeview menu-open">
+                    <a href="#" class="nav-link active">
+                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <p>
+                            Starter Pages
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link active">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Active Page</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Inactive Page</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <?= HTML::a(' UserManager', ['/user/index'], ['data-method' => 'post', 'class' => 'nav-link']) ?>
+                </li>
+                <li class="nav-item">
+                    <?= Html::a('Sign out', ['site/logout'], ['data-method' => 'post', 'class' => 'nav-link']) ?>
+                </li>
+            </ul>
         </nav>
         <!-- /.sidebar-menu -->
     </div>
--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -1,11 +1,24 @@
 <?php
+
+use hail812\adminlte3\widgets\Alert;
+use hail812\adminlte3\widgets\Callout;
+
 $this->title = 'Zona Administrativa';
 $this->params['breadcrumbs'] = [['label' => $this->title]];
 ?>
 <div class="container-fluid">
     <div class="row">
         <div class="col-lg-6">
-            
+            <?= Alert::widget([
+                'type' => 'success',
+                'body' => '<h3>Welcome!!!</h3>',
+            ]) ?>
+
+            <?= Callout::widget([
+                'type' => 'danger',
+                'head' => 'I am a danger callout!',
+                'body' => 'There is a problem that we need to fix. A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.'
+            ]) ?>
         </div>
     </div>
 </div>
