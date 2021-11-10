84c03a818807ee3d2b4f9bf78b5e0f8c715ad961
Tiago Gil <73337790+itzgunny@users.noreply.github.com>
2021-11-10 10:21:14 +0000

    Drop-down list

--- backend/controllers/UserController.php
+++ backend/controllers/UserController.php
@@ -6,6 +6,7 @@ namespace backend\controllers;
 use app\models\User;
 use backend\models\UserCreateForm;
 use Yii;
+use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
